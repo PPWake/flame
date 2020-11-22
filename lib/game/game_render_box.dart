21ff5a6d7ad32ee8a5b9dd99268732fdfa7827d7
renancaraujo <renan.araujo@dashdash.com>
2020-11-22 18:31:52 +0000

    remove unused offset

--- lib/game/game_render_box.dart
+++ lib/game/game_render_box.dart
@@ -62,7 +62,7 @@ class GameRenderBox extends RenderBox with WidgetsBindingObserver {
   @override
   void paint(PaintingContext context, Offset offset) {
     context.canvas.save();
-    final delta = game.builder.offset + offset;
+    final delta = offset;
     context.canvas.translate(delta.dx, delta.dy);
     game.render(context.canvas);
     context.canvas.restore();
